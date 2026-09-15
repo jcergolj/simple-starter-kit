3eeddeb6218fe3df5ada3ea0c9eb860bcd386739
jcergolj <me@jcergolj.me.uk>
2026-09-15 13:48:22 +0200

    Fix SFTP fingerprint environment handling

--- config/filesystems.php
+++ config/filesystems.php
@@ -69,7 +69,7 @@ return [
             'password' => env('SFTP_PASSWORD'),
             'privateKey' => env('SFTP_PRIVATE_KEY'),
             'passphrase' => env('SFTP_PASSPHRASE'),
-            'hostFingerprint' => env('SFTP_HOST_FINGERPRINT'),
+            'hostFingerprint' => getenv('SFTP_HOST_FINGERPRINT') ?: env('SFTP_HOST_FINGERPRINT') ?: null,
             'visibility' => 'private',
             'directory_visibility' => 'private',
             'port' => (int) env('SFTP_PORT', 22),
